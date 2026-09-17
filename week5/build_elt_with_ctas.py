fb183abfd5797070a75cda05f635db918e0f4706
Keeyong <keeyonghan@hotmail.com>
2026-09-17 16:41:08 -0700

    Update build_elt_with_ctas.py

--- week5/build_elt_with_ctas.py
+++ week5/build_elt_with_ctas.py
@@ -10,12 +10,9 @@ import logging
 import snowflake.connector
 
 """
-This pipeline assumes that there are two other tables in your snowflake DB
+This pipeline populates two tables first in your snowflake DB
  - user_session_channel
  - session_timestamp
-
-With regard to how to set up these two tables, please refer to this README file:
- - https://github.com/keeyong/sjsu-data226-FA25/blob/main/week8/How-to-setup-ETL-tables-for-ELT.md
 """
 
 def return_snowflake_conn():
