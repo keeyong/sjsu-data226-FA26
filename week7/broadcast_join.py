fc505b5c709c38fea25bf885f031ec82c0111f4f
Keeyong <keeyonghan@hotmail.com>
2026-09-19 19:35:55 -0700

    Add files via upload

---
+++ week7/broadcast_join.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast
+
+spark = SparkSession \
+    .builder \
+    .appName("Broadcast Join Demo") \
+    .master("local[3]") \
+    .config("spark.sql.shuffle.partitions", 3) \
+    .config("spark.sql.adaptive.enabled", False) \
+    .getOrCreate()
+
+df_large = spark.read.json("large_data/")
+df_small = spark.read.json("small_data")
+
+import time
+
+# Start the timer
+start = time.perf_counter()
+join_expr = df_large.id == df_small.id
+join_df = df_large.join(broadcast(df_small), join_expr, "inner")
+
+join_df.collect()
+# End the timer and calculate duration
+end = time.perf_counter()
+print(f"Broadcast Join Execution Time: {end - start:.4f} seconds")
+input("Waiting ...")
+
+spark.stop()
